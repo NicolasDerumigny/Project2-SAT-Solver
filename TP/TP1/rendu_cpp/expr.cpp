064118ff4cdd38fdc7918e78634eba1d9638cff0
Derumigny Nicolas <nderumig@slsu0-10.dsi.ens-lyon.fr>
2016-02-03 11:55:09 +0100

    Modification du TP1

--- TP/TP1/rendu_cpp/expr.cpp
+++ TP/TP1/rendu_cpp/expr.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-/***********************************/
+/*************************************/
 /**********  Constants   ***********/
 /***********************************/
 
@@ -11,7 +11,7 @@ EConst::EConst(int val) : value(val) {}
 string EConst::to_string()
 {
     ostringstream oss;
-    oss << value;
+    oss << "v" << value;
     string result = oss.str();
     return result;
 }
@@ -21,35 +21,118 @@ int EConst::eval()
     return value;
 }
 
-/***********************************/
-/**********  Additions   ***********/
-/***********************************/
+/****************************************/
+/**********  Conjonctions   ***********/
+/***************************************/
 
-EAdd::EAdd(Expr * e1, Expr * e2) : op1(e1), op2(e2) {}
+EConj::EConj(Expr * e1, Expr * e2) : op1(e1), op2(e2) {}
 
-string EAdd::to_string()
+string EConj::to_string()
 {
-    return "(" + op1->to_string() + " + " +  op2->to_string() + ")";
+    return "(" + op1->to_string() + " /\ " +  op2->to_string() + ")";
 }
 
-int EAdd::eval()
+int EConj::eval()
 {
     return op1->eval() + op2->eval();
 }
 
-/***********************************/
-/********  Multiplications   *******/
+/************************************/
+/********  Disjonctions   **********/
 /***********************************/
 
-EMul::EMul(Expr * e1, Expr * e2) : op1(e1), op2(e2) {}
+EDisj::EDisj(Expr * e1, Expr * e2) : op1(e1), op2(e2) {}
 
-string EMul::to_string()
+string EDisj::to_string()
 {
-    return "(" + op1->to_string() + " x " +  op2->to_string() + ")";
+    return "(" + op1->to_string() + " \/ " +  op2->to_string() + ")";
 }
 
-int EMul::eval()
+int EDisj::eval()
 {
     return op1->eval() * op2->eval();
+}
+
+/************************************/
+/********  XOR  *******************/
+/***********************************/
+
+EXor::EXor(Expr * e1, Expr * e2) : op1(e1), op2(e2) {}
+
+string EXor::to_string()
+{
+    return "(" + op1->to_string() + " X " +  op2->to_string() + ")";
+}
+
+int EXor::eval()
+{
+    return op1->eval() * op2->eval();
+}
+
+/************************************/
+/********  IMPLY   ****************/
+/***********************************/
+
+EImply::EImply(Expr * e1, Expr * e2) : op1(e1), op2(e2) {}
+
+string EImply::to_string()
+{
+    return "(" + op1->to_string() + " → " +  op2->to_string() + ")";
+}
+
+int EImply::eval()
+{
+    return op1->eval() * op2->eval();
+}
+
+/************************************/
+/********  EQ   ********************/
+/***********************************/
+
+EEq::EEq(Expr * e1, Expr * e2) : op1(e1), op2(e2) {}
+
+string EEq::to_string()
+{
+    return "(" + op1->to_string() + " ↔ " +  op2->to_string() + ")";
+}
+
+int EEq::eval()
+{
+    return op1->eval() * op2->eval();
+}
+
+/***********************************/
+/********  ENOT  *****************/
+/***********************************/
+
+ENot::ENot(Expr * e1) : op1(e1) {}
+
+string ENot::to_string()
+{
+    return "¬(" + op1->to_string() + ")";
+}
+
+int ENot::eval()
+{
+    return op1->eval();
+}
+
+/***********************************/
+/********  VNOT                *******/
+/***********************************/
+
+VNot::VNot(int val) : value(val) {}
+
+string VNot::to_string()
+{
+    ostringstream oss;
+    oss << "¬v" << value;
+    string result = oss.str();
+    return result;
+}
+
+int VNot::eval()
+{
+    return value;
 }
 
