0d1bd811a4c35449db567cb6daebde3cb0d3fdac
Nicolas Derumigny <nderumigny@gmail.com>
2016-02-18 15:17:04 +0100

    Pre-debug 2

---
+++ Rendu1/assignation.cpp
@@ -0,0 +1,23 @@
+#include "assignation.h"
+
+void assignation::set_assign(var* variable,bool bet) {
+	this->variable=variable;
+	this->bet=bet;
+}
+
+void assignation::updateLitt(bool alive){
+	for (auto& cl:this->variable->clauseInto)
+		if (alive == false) { //si on tue une variable, on recherche les littéraux associés dans les éléments vivants et on les transfères vers les morts.
+			for (auto& li:cl->mElementAlive)//si un littéral (donc la variable) est déjà mort on ne fait rien.
+				if (li.second != nullptr && li.second->variable == this->variable) {
+					cl->mElementDead[li.first] = li.second;
+					li.second = nullptr;
+				}
+		} else { //et réciproquement...
+			for (auto& li:cl->mElementDead)
+				if (li.second != nullptr && li.second->variable == this->variable) {
+					cl->mElementAlive[li.first] = li.second;
+					li.second = nullptr;
+				}
+		}
+}
