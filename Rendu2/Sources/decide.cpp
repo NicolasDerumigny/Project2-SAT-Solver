4f05013ac2bd6fea19a1601a0ed90bc590d08721
MonegoEtmoi <ruben.staub@ens-lyon.fr>
2016-03-04 01:00:52 +0100

    Random new variable implemented

--- Rendu2/Sources/decide.cpp
+++ Rendu2/Sources/decide.cpp
@@ -1,15 +1,37 @@
 #include <vector>
 #include "../Header/assignation.h"
-//#include <stdlib.h>
-//#include <time.h>
+#include <stdlib.h>
+#include <time.h>
 
 var* getFreeVar() {
-	for (auto& s:v_var) {
+	for (var* s:v_var) {
         if (s != nullptr && s->value == -1) {
 			return s;
 		}
 	}
 	return nullptr;
+}
+
+var* getRandFreeVar() {
+	srand(time(NULL));
+	var* new_var = nullptr;
+	bool no_more = true;
+	for (var* s:v_var) {
+		if (s != nullptr && s->value == -1) {
+			no_more = false;
+			break;
+		}
+	}
+	if (no_more)
+		return nullptr;
+	unsigned int i_var;
+	while (new_var == nullptr) {
+		i_var = rand() % v_var.size();
+		if (v_var[i_var] != nullptr && v_var[i_var]->value == -1) {
+			new_var = v_var[i_var];
+		}
+	}
+	return new_var;
 }
 
 //voir var::assignValue avec value=1 par défaut et bet=true
