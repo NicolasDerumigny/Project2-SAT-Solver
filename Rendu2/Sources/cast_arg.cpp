af5d0662aaf34a357b243b293575d161622c141c
MonegoEtmoi <ruben.staub@ens-lyon.fr>
2016-03-09 11:14:43 +0100

    Debug + gprof

--- Rendu2/Sources/cast_arg.cpp
+++ Rendu2/Sources/cast_arg.cpp
@@ -1,7 +1,7 @@
 #include <string.h>
 
 bool is_tseitin(int argc, char** argv){
-    for (int i=1; i<=argc;i++)
+	for (int i=1; i<argc;i++)
         if (strncmp(argv[i],"-tseitin",8) and (strlen(argv[i])==8) )
             return true;
     return false;
