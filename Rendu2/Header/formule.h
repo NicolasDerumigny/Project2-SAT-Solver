77d8d66bf20cab439139a92227ddd113fc7b25f6
MonegoEtmoi <ruben.staub@ens-lyon.fr>
2016-03-20 14:11:30 +0100

    prepocessing definition

--- Rendu2/Header/formule.h
+++ Rendu2/Header/formule.h
@@ -9,14 +9,15 @@ public:
     void set_formule_tseitin(int varid, bool neg);
     void print();
     void free_formule();
+	void preprocessing();
 
 	clause* f_ClauseSatisfied;
 	clause* f_ClauseUnsatisfied;
 	clause* l_ClauseSatisfied;
 	clause* l_ClauseUnsatisfied;
 
-    map<unsigned int,clause*> mClauseSatisfied;
-    map<unsigned int,clause*> mClauseUnsatisfied;
+//    map<unsigned int,clause*> mClauseSatisfied;
+//    map<unsigned int,clause*> mClauseUnsatisfied;
     void merge(formule* formule2);
     //merge this and formule 2 and place it into this
 };
